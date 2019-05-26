bbb77128018734c33db19795c6ec5a943b35042e
GoatFeatherFarm <44263291+missybarringer@users.noreply.github.com>
2019-05-26 09:52:07 -0500

    added routes, style, & layouts

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // require dependencies
 var express = require("express");
+var mongoose = require("mongoose");
 var expressHandlebars = require("express-handlebars");
 var bodyParser = require("body-parser");
 
@@ -11,6 +12,9 @@ var app = express();
 
 // set up an express router
 var router = express.Router();
+
+// require our routes file pass our router object
+require("./config/routes")(router);
 
 // designate public folder as a static directory
 app.use(express.static(__dirname + "/public"));
@@ -28,6 +32,20 @@ app.use(bodyParser.urlencoded({
 
 // have every request go through our router middleware
 app.use(router);
+// If deployed, use deployed db else use local mongoHeadlines db
+var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Connect mongoose to out database
+mongoose.connect(db, function(error) {
+    //Log any errors connecting with mongoose
+    if (error) {
+        console.log(error);
+    }
+    // or log a success message
+    else {
+        console.log("mongoose connection is successful");
+    }
+});
 
 // listen on the port
 app.listen(PORT, function() {
