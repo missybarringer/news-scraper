e0165658e057b15bfb54e54e2cf489eb44cca12e
GoatFeatherFarm <44263291+missybarringer@users.noreply.github.com>
2019-05-30 15:24:07 -0500

    added some visual style & getting ready to deploy to heroku

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -3,7 +3,7 @@ var request = require("request");
 var cheerio = require("cheerio");
 
 var scrape = function (callback) {
-    request("http://www.echojs.com", function(err, res, body) {
+    request("http://www.nytimes.com/section/us", function(err, res, body) {
         var $ = cheerio.load(body);
 
         var articles = [];
@@ -11,10 +11,11 @@ var scrape = function (callback) {
         $("article h2").each(function(i, element) {
             var head = $(this).children("a").text().trim();
             var sum = $(this).children("a").attr("href").trim();
+            // var sum = $(this).children("p").text().trim();
 
             if(head && sum) {
                 var headNeat = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
-                var sumNeat = sum.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+                var sumNeat = "https://www.nytimes.com" + sum.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
 
                 var dataToAdd = {
                     headline: headNeat,
